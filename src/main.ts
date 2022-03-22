048e6220ada3d90f83a7271dcc00b0fa800039b0
elninocavalcanti10 <acn2011@outlook.com>
2022-03-22 17:14:14 -0300

    Add procfile e comando de deploy

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 8080;
+  const port = 8080;
   await app.listen(port);
   console.log(`App running on PORT: ${port}`);
 }

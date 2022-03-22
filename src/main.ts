b2bf83967bfac837e58bc821c629e04252b0a5cc
elninocavalcanti10 <acn2011@outlook.com>
2022-03-22 18:44:05 -0300

    port 8080

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 8081;
+  const port = 8080;
   await app.listen(port);
   console.log(`App running on PORT: ${port}`);
 }

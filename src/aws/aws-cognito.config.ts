6a80dfdd828d221531afe8bb50871ca62f8e6ad0
elninocavalcanti10 <acn2011@outlook.com>
2022-03-24 15:56:11 -0300

    Corrigindo erros

--- src/aws/aws-cognito.config.ts
+++ src/aws/aws-cognito.config.ts
@@ -9,7 +9,6 @@ export class AwsCognitoConfig {
 
     public userPoolId = this.configService.get<string>('COGNITO_USER_POOL_ID');
     public clientId = this.configService.get<string>('COGNITO_CLIENT_ID');
-    public region = this.configService.get<string>('AWS_REGION_US_VIRGINIA');
-    // public authority = `https://cognito-idp.${this.region}.amazonaws.com/${this.userPoolId}`;
-    public authority = 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_oUSdpvICF';
+    public region = this.configService.get<string>('AWS_REGION_US_OREGON');
+    public authority = `https://cognito-idp.${this.region}.amazonaws.com/${this.userPoolId}`;
 }

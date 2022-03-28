d04ac8be35078a70eeaed1dc8948cc81d4001136
elninocavalcanti10 <acn2011@outlook.com>
2022-03-28 18:34:07 -0300

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   const port = 8080;
   await app.listen(port);
   console.log(`App Legend running: ${port}`);

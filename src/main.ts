78396bf71b8b3e4d669e3c400a4e5d24ff62ac27
elninocavalcanti10 <acn2011@outlook.com>
2022-03-29 14:47:20 -0300

    Cors 2

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,19 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method == "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+ 
+    next();
+  });
   const port = 8080;
   await app.listen(port);
   console.log(`App Legend running: ${port}`);
